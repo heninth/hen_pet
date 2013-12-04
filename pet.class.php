a90c93c7b1b5792dd5ebd17f4ca05a5e4605dc71
heninth <heninth@hen.in.th>
2013-12-04 20:26:49 +0700

    fixed - error when fetch on anonymous post

--- pet.class.php
+++ pet.class.php
@@ -12,6 +12,7 @@ class plugin_hen_pet_forum extends plugin_hen_pet {
 		if($_G['cache']['plugin']['hen_pet']['open']==2){return $output;}
 		$i = 0;
 		foreach($postlist as $p){
+			$p['authorid'] = intval($p['authorid']);
 			if($pet = DB::fetch_first("SELECT n.*,m.image AS image, m.name AS name FROM ".DB::table('hen_mypet')." n LEFT JOIN ".DB::table('hen_petshop')." m ON n.pid=m.pid WHERE n.uid = {$p['authorid']} AND current = 1")){
 				$output[$i] = '&nbsp;&nbsp;&nbsp;<b>Pet:</b><center><div style="width:140px;"><strong>'.$pet['name'].'</strong><br>'.$pet['text'].'<br><img src="'.$pet['image'].'" /></div></div><br /><span title="Copyright hen &amp; new">&copy;</span></center>';
 			}else{
